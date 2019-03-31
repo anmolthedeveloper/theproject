eb37685de32a08d028e079022ec0b45c834f4b13
Anmol Singh <anmolthedeveloper@gmail.com>
2019-03-31 10:52:26 +0530

    Created 3 functions input(), calc() and output() to calculate factorial

--- main.cpp
+++ main.cpp
@@ -2,8 +2,35 @@
 
 using namespace std;
 
+class factorial{
+private:
+    int f, inp;
+public:
+    void input();
+    void calc();
+    void output();
+};
+
+void factorial::input(){
+    cout<<"Enter a number to get it's factorial : ";
+    cin>>inp;
+}
+
+void factorial::calc(){
+    f=1;
+    for(int i=inp;i>0;i--)
+        f*=i;
+}
+
+void factorial::output(){
+    cout<<endl<<"Factorial of "<<inp<<" is "<<f;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    factorial f;
+    f.input();
+    f.calc();
+    f.output();
     return 0;
 }
